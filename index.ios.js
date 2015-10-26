665de06b20e698fcd606f23b775f7b7f09af230a
Ants <anto.aravinth.cse@gmail.com>
2015-10-26 16:41:20 +0530

    Data storgae for wether input

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,9 @@
 'use strict';
 
 var React = require('react-native');
+var AsyncStorage = require('react-native').AsyncStorage;
+var _ = require("lodash");
+
 var {
   AppRegistry,
   StyleSheet,
@@ -33,19 +36,37 @@ var ForeCast = React.createClass({
 
 var WeatherProject = React.createClass({
 
+  componentDidMount : function() {
+      AsyncStorage.multiGet(['pincode'], (err,val) => {
+          var obj = _.zipObject(val)
+          var input = this.refs.pincode
+          this.setState({
+              pincode : obj['pincode']
+          })
+      })
+  },
+
   getInitialState : function() {
       return {
          margin : 25,
-         forecast: null
+         forecast: null, 
+         pincode : null
       }
   },
 
     _handleTextChange: function(event) {
     var zip = event.nativeEvent.text;
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='
+    //http://api.openweathermap.org/data/2.5/weather?APPID=b222d50e3930d42b59b15b28f38553e2&q=600002
+    fetch('http://api.openweathermap.org/data/2.5/weather?APPID=b222d50e3930d42b59b15b28f38553e2&q='
       + zip + '&units=imperial')
       .then((response) => response.json())
       .then((responseJSON) => {
+        AsyncStorage.multiSet([
+            ['pincode',zip]
+        ],(err) => {
+          if(err)
+            throw err;
+        })
         this.setState({
           forecast: {
             main: responseJSON.weather[0].main,
@@ -73,7 +94,7 @@ var WeatherProject = React.createClass({
         <Text style={styles.welcome}>
           Enter your zip code
         </Text>
-        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1, margin: this.state.margin }} placeholder="ZipCode" 
+        <TextInput ref="pincode" value={this.state.pincode} onChangeText={(text) => this.setState({ pincode: text })} style={{height: 40, borderColor: 'gray', borderWidth: 1, margin: this.state.margin }} placeholder="ZipCode" 
           onSubmitEditing={this._handleTextChange}/>
         {foreCastContent}
       </View>
